e90bcb98123c182934cfdd5510708e9014180ffa
Sokos6 <will.sokolowski@gmail.com>
2019-12-13 00:44:05 -0600

    approval update and className fix

--- src/index.js
+++ src/index.js
@@ -7,25 +7,38 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sur"
-        timeAgo="Today at 445"
-        userComment="hey"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 2am"
-        userComment="Go"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Mer"
-        timeAgo="Yesterday at 2pm"
-        userComment="Crazy"
-        avatar={faker.image.avatar()}
-      />
+
+    <ApprovalCard>
+      <div>
+        <h4>Warning!</h4>
+        Are you sure you want to do this?
+      </div>
+    </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sur"
+          timeAgo="Today at 445"
+          userComment="hey"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 2am"
+          userComment="Go"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Mer"
+          timeAgo="Yesterday at 2pm"
+          userComment="Crazy"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
